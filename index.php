df5e18102d78642adb59c9032d1198257efe23b9
SparksCode <pmonserratsoto@gmail.com>
2023-01-02 10:47:38 +0100

    Modificación del archivo añadiendo una función de suma

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>PEDRO</h1>
+    <h1>MONSERRAT</h1>
+
+
+    <?php
+    //PHP inicial con la operación suma
+    function suma($a, $b)
+    {
+        return $a + $b;
+    }
+    ?>
+
 </body>
+
 </html>
